2e8debeb3b089949952dac88e5effba7597ca2b1
felixHPatzschke <felix.patzschke@gmail.com>
2016-05-15 16:54:23 +0200

    Did the first bit of testing on polynomials and complex numbers
    removed some plain syntax errors, and other mistakes that happen, when
    you don't write your code in a proper IDE
    As far as I can tell, the arithmetic operators, output overrides, and
    constructors for complex numbers all work, but the polynomials are still
    a mess.

--- util/tcomplex.hpp
+++ util/tcomplex.hpp
@@ -29,8 +29,8 @@
 
 enum class complex_representation : unsigned int { arithmetic = 1u, polar = 2u };
 #define CXREP complex_representation
-#define CXARITHMETIC complex_representation::arithmetic;
-#define CXPOLAR complex_representation::polar;
+#define CXARITHMETIC complex_representation::arithmetic
+#define CXPOLAR complex_representation::polar
 #define CXREAL 0u
 #define CXIMAG 1u
 
@@ -49,7 +49,7 @@ public:
 	}
 	inline Complex(const _ty x, const _ty y = _ty(0), const CXREP representation = CXARITHMETIC)
 	{
-		if(representation==CXARITHMETIC)
+		if (representation == CXARITHMETIC)
 		{
 			re = x;
 			im = y;
@@ -63,24 +63,24 @@ public:
 	template <typename aux> inline Complex(std::initializer_list<aux> list)
 	{
 		re = static_cast<_ty>(*(list.begin()));
-		im = static_cast<_ty>(*(list.begin()+1));
+		im = static_cast<_ty>(*(list.begin() + 1));
 	}
 	inline Complex(const Complex<_ty>& other)
 	{
 		re = other.re;
 		im = other.im;
 	}
-	#ifdef _STD_COMPLEX_INCLUDED_
+#ifdef _STD_COMPLEX_INCLUDED_
 	inline Complex(const std::complex&);
-	#endif
+#endif
 	/// destructor
 	inline ~Complex() {  }
 
-	#ifdef _STD_COMPLEX_INCLUDED_
+#ifdef _STD_COMPLEX_INCLUDED_
 	inline operator std::complex() const { return std::complex(re, im); }
-	#endif
+#endif
 	template <typename aux> inline Complex<aux> cast() const { return Complex<aux>(static_cast<aux>(re), static_cast<aux>(im), CXARITHMETIC); }
-	
+
 	/// direct assignment operators
 	inline Complex<_ty>& operator=(const Complex<_ty>& other)
 	{
@@ -91,7 +91,7 @@ public:
 	template <typename aux> inline Complex<_ty>& operator=(std::initializer_list<aux> list)
 	{
 		re = static_cast<_ty>(*(list.begin()));
-		im = static_cast<_ty>(*(list.begin()+1));
+		im = static_cast<_ty>(*(list.begin() + 1));
 		return *this;
 	}
 	inline Complex<_ty>& operator=(const _ty x)
@@ -111,7 +111,7 @@ public:
 	template <typename aux> inline Complex<_ty>& operator+=(std::initializer_list<aux> list)
 	{
 		re += static_cast<_ty>(*(list.begin()));
-		im += static_cast<_ty>(*(list.begin()+1));
+		im += static_cast<_ty>(*(list.begin() + 1));
 		return *this;
 	}
 	template <typename aux> inline Complex<_ty>& operator+=(const aux x)
@@ -119,9 +119,9 @@ public:
 		re += static_cast<_ty>(x);
 		return *this;
 	}
-	#ifdef _STD_COMPLEX_INCLUDED_
+#ifdef _STD_COMPLEX_INCLUDED_
 	inline Complex<_ty>& operator+=(const std::complex&);
-	#endif
+#endif
 	inline Complex<_ty>& operator-=(const Complex<_ty>& other)
 	{
 		re -= other.re;
@@ -131,7 +131,7 @@ public:
 	template <typename aux> inline Complex<_ty>& operator-=(std::initializer_list<aux> list)
 	{
 		re -= static_cast<_ty>(*(list.begin()));
-		im -= static_cast<_ty>(*(list.begin()+1));
+		im -= static_cast<_ty>(*(list.begin() + 1));
 		return *this;
 	}
 	template <typename aux> inline Complex<_ty>& operator-=(const aux x)
@@ -139,22 +139,22 @@ public:
 		re -= static_cast<_ty>(x);
 		return *this;
 	}
-	#ifdef _STD_COMPLEX_INCLUDED_
+#ifdef _STD_COMPLEX_INCLUDED_
 	inline Complex<_ty>& operator-=(const std::complex&);
-	#endif
+#endif
 	inline Complex<_ty>& operator*=(const Complex<_ty>& other)
 	{
-		_ty _re = (this->re*other.re)-(this->im*other.im);
-		_ty _im = (this->re*other.im)+(this->im*other.re);
+		_ty _re = (this->re*other.re) - (this->im*other.im);
+		_ty _im = (this->re*other.im) + (this->im*other.re);
 		this->re = _re;
 		this->im = _im;
 		return *this;
 	}
 	template <typename aux> inline Complex<_ty>& operator*=(std::initializer_list<aux> list)
 	{
-		std::initializer_list<aux>::iterator lre = list.begin(), lim = list.begin()+1;
-		_ty _re = (this->re * static_cast<_ty>(*lre))-(this->im * static_cast<_ty>(*lim));
-		_ty _im = (this->re * static_cast<_ty>(*lim))+(this->im * static_cast<_ty>(*lre));
+		std::initializer_list<aux>::iterator lre = list.begin(), lim = list.begin() + 1;
+		_ty _re = (this->re * static_cast<_ty>(*lre)) - (this->im * static_cast<_ty>(*lim));
+		_ty _im = (this->re * static_cast<_ty>(*lim)) + (this->im * static_cast<_ty>(*lre));
 		this->re = _re;
 		this->im = _im;
 		return *this;
@@ -165,21 +165,21 @@ public:
 		im *= static_cast<_ty>(x);
 		return *this;
 	}
-	#ifdef _STD_COMPLEX_INCLUDED_
+#ifdef _STD_COMPLEX_INCLUDED_
 	inline Complex<_ty>& operator*=(const std::complex&);
-	#endif
+#endif
 	inline Complex<_ty>& operator/=(const Complex<_ty>& other)
 	{
-		_ty a = this->re, b= this->im;
-		this->re = (a*(other.re)+b*(other.im))/((other.re)*(other.re)+(other.im)*(other.im));
-		this->im = (b*(other.re)-a*(other.im))/((other.re)*(other.re)+(other.im)*(other.im));
+		_ty a = this->re, b = this->im;
+		this->re = (a*(other.re) + b*(other.im)) / ((other.re)*(other.re) + (other.im)*(other.im));
+		this->im = (b*(other.re) - a*(other.im)) / ((other.re)*(other.re) + (other.im)*(other.im));
 		return *this;
 	}
 	template <typename aux> inline Complex<_ty>& operator/=(std::initializer_list<aux> list)
 	{
-		_ty a = this->re, b= this->im, c = static_cast<_ty>(*(list.begin())), d = static_cast<_ty>(*(list.begin()+1));
-		this->re = (a*c+b*d)/(c*c+d*d);
-		this->im = (b*c-a*d)/(c*c+d*d);
+		_ty a = this->re, b = this->im, c = static_cast<_ty>(*(list.begin())), d = static_cast<_ty>(*(list.begin() + 1));
+		this->re = (a*c + b*d) / (c*c + d*d);
+		this->im = (b*c - a*d) / (c*c + d*d);
 		return *this;
 	}
 	template <typename aux> inline Complex<_ty>& operator/=(const aux x)
@@ -188,89 +188,89 @@ public:
 		im /= static_cast<_ty>(x);
 		return *this;
 	}
-	#ifdef _STD_COMPLEX_INCLUDED_
+#ifdef _STD_COMPLEX_INCLUDED_
 	inline Complex<_ty>& operator/=(const std::complex&);
-	#endif
-	
+#endif
+
 	/// comparison operators
 	inline int operator==(const Complex<_ty>& other) const
 	{
-		return ((re==other.re)&&(im==other.im));
+		return ((re == other.re) && (im == other.im));
 	}
-	inline int operator!=(const Complex<_ty>& other) const;
+	inline int operator!=(const Complex<_ty>& other) const
 	{
-		return ((re!=other.re)||(im!=other.im));
+		return ((re != other.re) || (im != other.im));
 	}
 	template <typename aux> inline int operator==(const aux arg) const
 	{
-		return ((re==arg)&&(im==_ty(0)));
+		return ((re == arg) && (im == _ty(0)));
 	}
-	template <typename aux> inline int operator!=(const aux arg) const;
+	template <typename aux> inline int operator!=(const aux arg) const
 	{
-		return ((re!=arg)||(im!=_ty(0)));
+		return ((re != arg) || (im != _ty(0)));
 	}
-	#ifdef _STD_COMPLEX_INCLUDED_
+#ifdef _STD_COMPLEX_INCLUDED_
 	inline int operator==(const std::complex& stdc) const;
 	inline int operator!=(const std::complex& stdc) const;
-	#endif
+#endif
 
 	/// arithmetic operators
 	inline Complex<_ty> operator-() const { return Complex<_ty>(-re, -im) };
-	#ifdef _FHP_OVERLOAD_LOGICAL_NOT_OPERATOR_BY_COMPLEX_CONJUGATE_
+#ifdef _FHP_OVERLOAD_LOGICAL_NOT_OPERATOR_BY_COMPLEX_CONJUGATE_
 	inline Complex<_ty> operator!() const { return Complex<_ty>(re, -im) };
-	#endif
+#endif
 
 	inline Complex<_ty> operator+(const Complex<_ty>& other) const
 	{
-		return Complex<_ty>(this->re+other.re, this->im+other.im, CXARITHMETIC);
+		return Complex<_ty>(this->re + other.re, this->im + other.im, CXARITHMETIC);
 	}
 	template <typename aux> inline Complex<_ty> operator+(std::initializer_list<aux> list) const
 	{
-		return Complex<_ty>(this->re+static_cast<_ty>(*(list.begin())), this->im+static_cast<_ty>(*(list.begin()+1)), CXARITHMETIC);
+		return Complex<_ty>(this->re + static_cast<_ty>(*(list.begin())), this->im + static_cast<_ty>(*(list.begin() + 1)), CXARITHMETIC);
 	}
 	template <typename aux> inline Complex<_ty> operator+(const aux x) const
 	{
 		return Complex<_ty>(this->re + x, this->im, CXARITHMETIC);
 	}
-	#ifdef _STD_COMPLEX_INCLUDED_
+#ifdef _STD_COMPLEX_INCLUDED_
 	inline Complex<_ty> operator+(const std::complex&) const;
-	#endif
+#endif
 	inline Complex<_ty> operator-(const Complex<_ty>& other) const
 	{
-		return Complex<_ty>(this->re-other.re, this->im-other.im, CXARITHMETIC);
+		return Complex<_ty>(this->re - other.re, this->im - other.im, CXARITHMETIC);
 	}
 	template <typename aux> inline Complex<_ty> operator-(std::initializer_list<aux> list) const
 	{
-		return Complex<_ty>(this->re-static_cast<_ty>(*(list.begin())), this->im-static_cast<_ty>(*(list.begin()+1)), CXARITHMETIC);
+		return Complex<_ty>(this->re - static_cast<_ty>(*(list.begin())), this->im - static_cast<_ty>(*(list.begin() + 1)), CXARITHMETIC);
 	}
 	template <typename aux> inline Complex<_ty> operator-(const aux x) const
 	{
 		return Complex<_ty>(this->re - x, this->im, CXARITHMETIC);
 	}
-	#ifdef _STD_COMPLEX_INCLUDED_
+#ifdef _STD_COMPLEX_INCLUDED_
 	inline Complex<_ty> operator-(const std::complex&) const;
-	#endif
+#endif
 	inline Complex<_ty> operator*(const Complex<_ty>&) const;
 	template <typename aux> inline Complex<_ty> operator*(std::initializer_list<aux>) const;
 	template <typename aux> inline Complex<_ty> operator*(const aux x) const
 	{
 		return Complex<_ty>(x*re, x*im);
 	}
-	#ifdef _STD_COMPLEX_INCLUDED_
+#ifdef _STD_COMPLEX_INCLUDED_
 	inline Complex<_ty> operator*(const std::complex&) const;
-	#endif
+#endif
 	inline Complex<_ty> operator/(const Complex<_ty>&) const;
 	template <typename aux> inline Complex<_ty> operator/(std::initializer_list<aux>) const;
 	template <typename aux> inline Complex<_ty> operator/(const aux x) const
 	{
-		return Complex<_ty>(re/x, im/x);
+		return Complex<_ty>(re / x, im / x);
 	}
-	#ifdef _STD_COMPLEX_INCLUDED_
+#ifdef _STD_COMPLEX_INCLUDED_
 	inline Complex<_ty> operator/(const std::complex&) const;
-	#endif
+#endif
 
 	/// other operations
-	inline Complex<_ty> sqr() const { return Complex<_t>(re*re-im*im, _ty(2)*re*im); }
+	inline Complex<_ty> sqr() const { return Complex<_t>(re*re - im*im, _ty(2)*re*im); }
 	inline Complex<_ty> sqrt() const;
 
 	/// direct access
@@ -278,14 +278,14 @@ public:
 	inline _ty imag() const { return im; }
 	inline _ty& operator[](unsigned int i)
 	{
-		if(i==CXREAL) return re;
-		else if(i==CXIMAG) return im;
+		if (i == CXREAL) return re;
+		else if (i == CXIMAG) return im;
 		else return _ty(0);
 	}
 	inline _ty* get(const CXREP representation = CXARITHMETIC)
 	{
-		_ty* res = static_cast<_ty*>malloc(2*sizeof(_ty));
-		if(representation == CXARITHMETIC)
+		_ty* res = static_cast<_ty*>malloc(2 * sizeof(_ty));
+		if (representation == CXARITHMETIC)
 		{
 			res[0] = re;
 			res[1] = im;
@@ -301,7 +301,7 @@ public:
 	inline Complex<_ty>& set_imag(const _ty y) { im = y; return *this; }
 	inline Complex<_ty>& set(const _ty x, const _ty y, const CXREP representation = CXARITHMETIC)
 	{
-		if(representation==CXARITHMETIC)
+		if (representation == CXARITHMETIC)
 		{
 			re = x;
 			im = y;
@@ -313,19 +313,21 @@ public:
 		}
 		return *this;
 	}
-	
+
 	inline _ty abs() const { return sqrt(re*re + im*im); }
 	inline _ty arg() const { return atan2(im, re); }
 	inline _ty norm() const { return (re*re + im*im); }
 	inline Complex<_ty> conj() const { return Complex<_ty>(re, -im); }
-	
-	
+
+
 
 #ifdef _STD_STRING_INCLUDED_
 	inline std::string to_string() const
 	{
 		std::stringstream sstr();
-		sstr << re << "+" << im << "i";
+		sstr << re;
+		((im > _ty(0)) ? (sstr << "+" << im) : (sstr << "-" << -im));
+		sstr << "i";
 		return sstr.str();
 	}
 #endif
@@ -335,11 +337,11 @@ public:
 /// some more arithmetic operators
 template <typename _ty> inline Complex<_ty> operator+(_ty a, Complex<_ty>& z)
 {
-	return Complex<_ty>(a+z.real(), z.imag(), CXARITHMETIC);
+	return Complex<_ty>(a + z.real(), z.imag(), CXARITHMETIC);
 }
 template <typename _ty> inline Complex<_ty> operator-(_ty a, Complex<_ty>& z)
 {
-	return Complex<_ty>(a-z.real(), -z.imag(), CXARITHMETIC);
+	return Complex<_ty>(a - z.real(), -z.imag(), CXARITHMETIC);
 }
 template <typename _ty> inline Complex<_ty> operator*(_ty a, Complex<_ty>& z)
 {
@@ -347,7 +349,7 @@ template <typename _ty> inline Complex<_ty> operator*(_ty a, Complex<_ty>& z)
 }
 template <typename _ty> inline Complex<_ty> operator/(_ty a, Complex<_ty>& z)
 {
-	Complex<_ty> res(a,_ty(0));
+	Complex<_ty> res(a, _ty(0));
 	res /= z;
 	return res;
 }
@@ -361,7 +363,9 @@ template <typename _ty> inline _ty abs(const Complex<_ty>& z)
 /// output override
 template <typename _ty> inline std::ostream& operator<<(std::ostream& ostr, Complex<_ty>& z)
 {
-	return (ostr << z.real() << "+" << z.imag() << "i");
+	ostr << z.real();
+	((z.imag() > _ty(0)) ? (ostr << "+" << z.imag()) : (ostr << "-" << -(z.imag())));
+	return (ostr << "i");
 }
 #endif
 
--- util/tpolynomial.hpp
+++ util/tpolynomial.hpp
@@ -34,7 +34,7 @@ protected:
 
 public:
 	/** creates a new empty polynomial */
-	inline Polynomial()	{ coef = std::vector<_ty>(0); }
+	inline Polynomial() { coef = std::vector<_ty>(0); }
 	/** creates a new polynomial with given coefficients */
 	template <typename aux> inline Polynomial(std::initializer_list<aux> list)
 	{
@@ -42,44 +42,39 @@ public:
 		for (aux x : list)
 			coef.push_back(_ty(x));
 	}
-	/** creates a new polynomial of i-th degree */
-	inline Polynomial(const unsigned int i)
-	{
-		coef = std::vector<_ty>(i+1);
-	}
 	/** creates a new polynomial of i-th degree, and inserts a leading coefficient */
 	inline Polynomial(const unsigned int i, const _ty leading)
 	{
-		coef = std::vector<_ty>(i+1);
+		coef = std::vector<_ty>(i + 1);
 		coef[i] = leading;
 	}
 	/** creates a new 0th degree polynomial, and inserts a coefficient as the constant value */
-	inline Polynomial(const _ty i) 
-	{ 
-		coef = std::vector<_ty>(1); 
-		coef[0] = i; 
+	inline Polynomial(const _ty i)
+	{
+		coef = std::vector<_ty>(1);
+		coef[0] = i;
 	}
 	/** copy constructor */
 	inline Polynomial(const Polynomial<_ty>& other)
 	{
 		coef = std::vector<_ty>(other.coef.size());
-		for(unsigned int i=0; i<coef.size(); ++i)
+		for (unsigned int i = 0; i < coef.size(); ++i)
 			coef[i] = other.coef[i];
 	}
 	/** destructor */
-	inline ~Polynomial(){}
+	inline ~Polynomial() {}
 
 	inline Polynomial<_ty>& operator=(const Polynomial<_ty>& other)
 	{
 		coef.resize(other.coef.size())
-		for(unsigned int i=0; i<coef.size(); ++i)
-			coef[i] = other.coef[i];
+			for (unsigned int i = 0; i < coef.size(); ++i)
+				coef[i] = other.coef[i];
 		return *this;
 	}
 	template <typename aux> inline Polynomial<_ty>& operator=(std::initializer_list<aux> list)
 	{
 		coef.resize(0);
-		for(aux x : list)
+		for (aux x : list)
 			coef.push_back(_ty(x));
 		return *this;
 	}
@@ -88,24 +83,24 @@ public:
 		coef.resize(1);
 		coef[0] = scalar;
 	}
-	
+
 	inline Polynomial<_ty> operator-() const
 	{
-		Polynomial<_ty> res = Polynomial(coef.size()-1);
-		for(unsigned int i=0; i<coef.size(); ++i)
+		Polynomial<_ty> res = Polynomial(coef.size() - 1);
+		for (unsigned int i = 0; i < coef.size(); ++i)
 			res.coef[i] = _ty(-1)*coef[i];
 		return res;
 	}
-	
+
 	inline Polynomial<_ty>& operator+=(const Polynomial<_ty>& other);
 	inline Polynomial<_ty>& operator+=(const _ty scalar);
-	
+
 	inline Polynomial<_ty>& operator-=(const Polynomial<_ty>& other);
 	inline Polynomial<_ty>& operator-=(const _ty scalar);
-	
+
 	inline Polynomial<_ty>& operator*=(const _ty scalar);
 	inline Polynomial<_ty>& operator/=(const _ty scalar);
-	
+
 	inline Polynomial<_ty>& operator*=(const Polynomial<_ty>& other);
 
 	inline Polynomial<_ty> operator+(const Polynomial<_ty>& other) const;
@@ -113,22 +108,22 @@ public:
 
 	inline Polynomial<_ty> operator-(const Polynomial<_ty>& other) const;
 	inline Polynomial<_ty> operator-(const _ty scalar) const;
-	
+
 	inline Polynomial<_ty> operator*(const _ty scalar) const;
 	inline Polynomial<_ty> operator/(const _ty scalar) const;
 
 	inline Polynomial<_ty> operator*(const Polynomial<_ty>& other) const;
 
-	
+
 	inline _ty operator()(const _ty x) const
 	{
 		_ty res = 0.0;
 		_ty x_pow = _ty(1);
-		for(unsigned int i=0; i<coef.size(); ++i)
+		for (unsigned int i = 0; i < coef.size(); ++i)
 		{
-			if(coef[i]!=_ty(0))
+			if (coef[i] != _ty(0))
 			{
-				_ty q = coef[i]*x_pow;
+				_ty q = coef[i] * x_pow;
 				res += q;
 			}
 			x_pow *= x;
@@ -141,7 +136,7 @@ public:
 		res.coef[i] = this->get_coefficient(i);
 		return res;
 	}
-	inline unsigned int degree() const { return coef.size()-1; }
+	inline int degree() const { return coef.size() - 1; }
 
 	inline Polynomial<_ty> derivative(const int n = 1) const;
 	inline _ty derivativeAt(const _ty x) const;
@@ -150,70 +145,72 @@ public:
 
 	inline _ty get_coefficient(const unsigned int i) const
 	{
-		if(i<coef.size())
+		if (i < coef.size())
 			return coef[i];
 		else
 			return _ty(0);
 	}
 	inline Polynomial<_ty>& set_coefficient(const unsigned int i, const _ty arg)
 	{
-		if(i<coef.size())
+		if (i < coef.size())
 			coef[i] = arg;
 		else
 		{
-			coef.resize(i+1);
+			coef.resize(i + 1);
 			coef[i] = arg;
 		}
 	}
-	inline std::vector<_ty>& coefficients()	{ return coef; }
+	inline std::vector<_ty>& coefficients() { return coef; }
 
-	#ifdef _STD_STRING_INCLUDED_
+#ifdef _STD_STRING_INCLUDED_
 	inline std::string to_string(const char* argn = "x") const
 	{
-		std::stringstream sstr();
+		if (coef.size() == 0)
+			return std::string("0");
+		std::stringstream sstr;
 		unsigned int x = 0;
-		for(unsigned int i=coef.size()-1; i>=0; --i)
+		for (int i = (coef.size() - 1); i >= 0; --i)
 		{
-			if(coef[i]!=_ty(0))
+			if (coef[i] != _ty(0))
 			{
-				if(x==0 && coef[i]<_ty(0))
+				if (x == 0 && coef[i] < _ty(0))
 					sstr << "-";
-				if(x!=0)
-					sstr << ((coef[i]>_ty(0))?(" + "):(" - "))
-				sstr << ((coef[i]>_ty(0))?(coef[i]):(-(coef[i])));
-				if(i!=0)
+				if (x != 0)
+					sstr << ((coef[i] > _ty(0)) ? (" + ") : (" - "));
+				sstr << ((coef[i] > _ty(0)) ? (coef[i]) : (-(coef[i])));
+				if (i != 0)
 					sstr << argn;
-				if(i>=2)
-					sstr << "^";
+				if (i >= 2)
+					sstr << "^" << i;
 				++x;
 			}
 		}
 		return sstr.str();
 	}
-	#endif
-	#ifdef _STD_OSTREAM_INCLUDED_
-	friend inline std::ostream& operator<<(std::ostream, const Polynomial<_ty>&)
-	#endif
+#endif
 
 };
 
 #ifdef _STD_OSTREAM_INCLUDED_
-inline std::ostream& operator<<(std::ostream ostr, const Polynomial<_ty>& pol)
+template <typename _ty>
+inline std::ostream& operator<<(std::ostream& ostr, const Polynomial<_ty>& pol)
 {
+	if (pol.degree() == -1)
+		return ostr << "0";
 	unsigned int x = 0;
-	for(unsigned int i=pol.coef.size()-1; i>=0; --i)
+	for (int i = pol.degree(); i >= 0; --i)
 	{
-		if(pol.coef[i]!=_ty(0))
+		if (pol.get_coefficient(i) != _ty(0))
 		{
-			if(x==0 && pol.coef[i]<_ty(0))
+			if (x == 0 && pol.get_coefficient(i) < _ty(0))
 				ostr << "-";
-			if(x!=0)
-				ostr << ((pol.coef[i]>_ty(0))?(" + "):(" - "))
-			ostr << ((pol.coef[i]>_ty(0))?(pol.coef[i]):(-(pol.coef[i])));
-			if(i!=0)
+			if (x != 0)
+				ostr << ((pol.get_coefficient(i) > _ty(0)) ? (" + ") : (" - "));
+			ostr << ((pol.get_coefficient(i) > _ty(0)) ? (pol.get_coefficient(i)) : (-(pol.get_coefficient(i))));
+			if (i != 0)
 				ostr << "x";
-			if(i>=2)
-				ostr << "^";
+			if (i >= 2)
+				ostr << "^" << i;
 			++x;
 		}
 	}
@@ -222,25 +219,25 @@ inline std::ostream& operator<<(std::ostream ostr, const Polynomial<_ty>& pol)
 #endif
 
 template <typename _ty>
-inline Polynomial<_ty> Polynomial<_ty>::derivative(const int n)
+inline Polynomial<_ty> Polynomial<_ty>::derivative(const int n) const
 {
-	if(n==0) 
+	if (n == 0)
 		return Polynomial<_ty>(*this)
-	else if(n<0)
+	else if (n < 0)
 		return integral(-n);
-	else if(n>degree())
+	else if (n > degree())
 		return Polynomial<_ty>(_ty(0));
-	else if(n==1)
+	else if (n == 1)
 	{
 		Polynomial<_ty> res(0u);
-		for(unsigned int i=1; i<coef.size(); ++i)
+		for (unsigned int i = 1; i < coef.size(); ++i)
 		{
-			res.set_coefficient(i-1, coef[i]*i);
+			res.set_coefficient(i - 1, coef[i] * i);
 		}
 		return res;
 	}
 	else
-		return derivative().derivative(n-1);
+		return derivative().derivative(n - 1);
 	// TODO: get rid of recursion
 }
 
